995dc1829052824008465342c4f5b59e564992e2
IKEBE Tomohiro <ikebe@shebang.jp>
2001-10-10 02:10:10 +0000

    Import of IKEBE/Mail-QmailQueue-0.02 from CPAN.
    gitpan-cpan-distribution: Mail-QmailQueue
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         IKEBE/Mail-QmailQueue-0.02.tar.gz
    gitpan-cpan-author:       IKEBE
    gitpan-cpan-maturity:     released

--- QmailQueue.pm
+++ QmailQueue.pm
@@ -1,109 +1,187 @@
 package Mail::QmailQueue;
-# $Id: QmailQueue.pm,v 1.2 2001/10/07 09:55:48 ikechin Exp $
+
 use strict;
 use vars qw($VERSION);
 use IO::Pipe;
 use IO::Handle;
+use Carp;
 
-$VERSION = '0.01';
+$VERSION = '0.02';
 
 sub new {
-  my $class = shift;
-  my $bin = shift || "/var/qmail/bin/qmail-queue";
-  my $self = {
-	      _bin => $bin,
-	      };
-  bless $self,$class;
+    my $class = shift;
+    my $bin = shift || "/var/qmail/bin/qmail-queue";
+    my $self = {
+		bin => $bin
+	       };
+    bless $self,$class;
 }
-
+  
 sub mail {
-  my $self = shift;
-  if ($_[0]) {
-    $self->{mail} = shift;
-  }
+    my $self = shift;
+    if ($_[0]) {
+	$self->{mail} = shift;
+    }
+    $self->{mail};
 }
 
-sub rcpt {
-  my $self = shift;
-  if (@_) {
-    if ($self->{rcpt}) {
-      push(@{$self->{rcpt}},@_);
+sub sender {
+    my $self = shift;
+    $self->mail(@_);
+}
+
+sub recipient {
+    my $self = shift;
+    if (@_) {
+	if ($self->{recipient}) {
+	    push(@{$self->{recipient}},@_);
+	} else {
+	    $self->{recipient} = [ @_ ];
+	}
     }
-    else{
-      $self->{rcpt} = [ @_ ];
-    }
-  }
+    $self->{recipient};
 }
 
 sub to {
-  my $self = shift;
-  $self->rcpt(@_);
+    my $self = shift;
+    $self->recipient(@_);
+}
+
+sub data {
+    my $self = shift;
+    if ($_[0]) {
+	$self->{data} = shift;
+    }
+    $self->{data} =~ s/\r\n/\n/g;
+    $self->{data};
 }
 
 sub datasend {
-  my $self = shift;
-  $self->{data} = shift;
-  $self->{data} =~ s/\r\n/\n/g;
-  $self->{data} .= "\n";
-  $self->_send;
+    my $self = shift;
+    $self->data(shift);
+    $self->send;
 }
 
-sub _send {
-  my $self = shift;
-  my $data = IO::Pipe->new;
-  my $addr = IO::Pipe->new;
-
-  if(my $pid = fork){
-    $data->writer;
-    $addr->writer;
-    $data->print($self->{data});
-    $data->close;
-    $addr->print("F" . $self->{mail} . "\0");
-    foreach my $rcpt (@{$self->{rcpt}}) {
-      $addr->print("T" . $rcpt . "\0");
+sub send {
+    my $self = shift;
+    my $data = IO::Pipe->new;
+    my $addr = IO::Pipe->new;
+    
+    foreach my $key (qw(recipient mail data)) {
+	if (!$self->{$key}) {
+	    carp "please specify $key";
+	}
     }
-    $addr->print("\0");
-    $addr->close;
-    wait;
-  } elsif (defined $pid) {
-    my $fd0 = IO::Handle->new->fdopen(0,"w");
-    my $fd1 = IO::Handle->new->fdopen(1,"w");
-    $data->reader();
-    $addr->reader();
-    $fd0->fdopen(fileno($data),"r");
-    $fd1->fdopen(fileno($addr),"r");
-    exec($self->{_bin}) || die $!;
-    exit;
-  }
-  else {
-    die "Cannot fork!!";
-  }
+    
+    if (my $pid = fork) {
+	$data->writer;
+	$addr->writer;
+	
+	$data->print($self->data);
+	$data->close;
+	
+	$addr->print("F" . $self->mail . "\0");
+	foreach my $rcpt (@{$self->recipient}) {
+	    $addr->print("T" . $rcpt . "\0");
+	}
+	$addr->print("\0");
+	$addr->close;
+	
+	delete $self->{mail};
+	delete $self->{recipient};
+	delete $self->{data};
+	
+	wait;
+    } elsif (defined $pid) {
+	my $fd0 = IO::Handle->new->fdopen(0,"w");
+	my $fd1 = IO::Handle->new->fdopen(1,"w");
+	$data->reader();
+	$addr->reader();
+	$fd0->fdopen(fileno($data),"r");
+	$fd1->fdopen(fileno($addr),"r");
+	exec($self->{bin}) || carp $!;
+	exit;
+    } else {
+	carp "Cannot fork!!";
+    }
 }
 
-sub data {1;};
-sub dataend {1;}
-sub quit {1;}
+
+sub quit {
+    # do nothing
+    1;
+}
 
 1;
 __END__
 
 =head1 NAME
 
-Mail::QmailQueue - Perl extension to using qmail-queue directry
+Mail::QmailQueue - Perl extension to operate qmail-queue directly
 
 =head1 SYNOPSIS
 
   use Mail::QmailQueue;
-  
-  
+  use Mime::Lite;
+
+  # generate mail.
+  my $mime = MIME::Lite->new(
+			     ...
+			    );
+
+  # send mail via qmail-queue.
+  my $qmail = Mail::QmailQueue->new;
+  $qmail->sender($ENV{USER});
+  $qmail->recipient('postmaster@foo.bar');
+  $qmail->data($mime->as_string);
+  $qmail->send;
 
 =head1 DESCRIPTION
 
-Stub documentation for Mail::QmailQueue, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+This module operate qmail-queue directly,
+so, you can send mail more faster than the case where SMTP is used.
 
-Blah blah blah.
+=head1 CONSTRUTOR
+
+=over 4
+
+=item new(QMAIL_QUEUE)
+
+construtor for Mail::QmailQueue object.
+QMAIL_QUEUE is location of qmail-queue program 
+(default /var/qmail/bin/qmail-queue)
+
+=back
+
+=head1 METHODS
+
+=over 4
+
+=item sender(ADDRESS)
+
+set sender's mail address.
+
+=item mail(ADDRESS)
+
+Synonym for sender.
+
+=item recipient(ADDRESS [,ADDRESS, [...]])
+
+set recipient's mail address.
+
+=item to(ADDRESS [,ADDRESS, [...]])
+
+Synonym for recipient.
+
+=item data(DATA)
+
+set mail message. (including header.)
+
+=item send
+
+send mail.
+
+=back
 
 =head1 AUTHOR
 
@@ -111,6 +189,13 @@ IKEBE Tomohiro E<lt>ikebe@edge.co.jpE<gt>
 
 =head1 SEE ALSO
 
-L<Net::SMTP>.
+qmail. http://cr.yp.to/qmail.html
+qmail-queue man page.
+
+=head1 COPYRIGHT
+
+Copyright(C) 2001 IKEBE Tomohiro All rights reserved.
+This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself. 
 
 =cut
+
